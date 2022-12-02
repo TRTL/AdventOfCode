6d3c3905c90fadd668fb2721a0ada4aa21f3b2c4
Karolis Stonys <4seven@gmail.com>
2022-12-02 21:27:09 +0200

    advent of code day 1 both tasks done

---
+++ AdventOfCode2022/AdventOfCode2022/Day_1/Day1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022.Day_1
+{
+    public class Day1
+    {
+        public int mostCalories = 0;
+        private int top1ElfsCalories = 0;
+        private int top2ElfsCalories = 0;
+        private int top3ElfsCalories = 0;
+
+        public int ReadFileAndGetMostCalories()
+        {
+            using StreamReader sr = new StreamReader("../../../Day_1/input.txt");
+            string newLine;
+            int newCalories = 0;
+
+            while ((newLine = sr.ReadLine()) != null)
+            {
+                if (newLine != "")
+                    newCalories += Convert.ToInt32(newLine);
+                else
+                {
+                    if (newCalories > mostCalories &&
+                        newCalories != top1ElfsCalories &&
+                        newCalories != top2ElfsCalories)
+                        mostCalories = newCalories;
+                    newCalories = 0;
+                }
+            }
+            return mostCalories;
+        }
+
+
+        public int ReadFileAndGetMostCaloriesForThreeElfs()
+        {
+            using StreamReader sr = new StreamReader("../../../Day_1/input.txt");
+            string newLine;
+
+            top1ElfsCalories = ReadFileAndGetMostCalories();
+            mostCalories = 0;
+            top2ElfsCalories = ReadFileAndGetMostCalories();
+            mostCalories = 0;
+            top3ElfsCalories = ReadFileAndGetMostCalories();
+
+            return  top1ElfsCalories + top2ElfsCalories + top3ElfsCalories;
+        }
+
+    }
+}
---
+++ AdventOfCode2022/AdventOfCode2022/Program.cs
@@ -0,0 +1,18 @@
+﻿using AdventOfCode2022.Day_1;
+
+namespace AdventOfCode2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Advent of code 2022!");
+
+            Day1 day1_result1 = new Day1();
+
+            Console.WriteLine(day1_result1.ReadFileAndGetMostCalories());
+            Console.WriteLine(day1_result1.ReadFileAndGetMostCaloriesForThreeElfs());
+            
+        }
+    }
+}
